49864f559e9d8236b7e4606a12e002b60f92d07c
aleios <dvsaleios@gmail.com>
2017-07-18 15:24:07 +1000

    Initial working script for foobar.php

--- foobar.php
+++ foobar.php
@@ -1,4 +1,28 @@
 <?php
 
+// Generate numbers 1 to 100
+for($num = 1; $num <= 100; $num++)
+{
+	if($num != 1)
+		echo ", ";
+	
+	// First test if number is both divisible by 3 and 5
+	if($num % 3 == 0 && $num % 5 == 0)
+	{
+		echo "foorbar";
+	}
+	else if($num % 3 == 0)
+	{
+		echo "foo";
+	}
+	else if($num % 5 == 0)
+	{
+		echo "bar";
+	}
+	else
+	{
+		echo $num;
+	}
+}
 
 ?>
